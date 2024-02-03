2ff90ebbae16d27dbea1a693ce237d7f650be3e9
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-02-03 08:26:53 +0000

    Create class Post and class Comment in models.py and create admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+# This will register both our post model and post admin class with our admin site
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
