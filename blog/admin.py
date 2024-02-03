e1e16c2ab0c976adcf94975335f874763f8e8a3e
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-02-03 19:45:20 +0000

    Create class CommentAdmin to display in admin panel and add actions to approve comment

--- blog/admin.py
+++ blog/admin.py
@@ -12,3 +12,15 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
