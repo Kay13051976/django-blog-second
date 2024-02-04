87a3c0163ecee44c2e290ee1d64a113ee4865138
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-02-04 22:00:01 +0000

    Add template variable in post_detail.html and Fix syntex error in views.py in blog app

--- blog/views.py
+++ blog/views.py
@@ -68,7 +68,7 @@ class PostDetail(View):
 
 class PostLike(View):
 
-    def post(self, request, slug):
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
 
         if post.likes.filter(id=request.user.id).exists():
@@ -76,4 +76,4 @@ class PostLike(View):
         else:
             post.likes.add(request.user)
 
-        return HttpResponseRedirect(reverse('post_detail', args[slug]))
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
